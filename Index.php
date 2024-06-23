4e15d9df151659e40c455ae78717b58c6df553eb
leosome2002 <leosomeone072@gmail.com>
2024-06-23 12:30:45 +0545

    footer change

--- Index.php
+++ Index.php
@@ -151,6 +151,7 @@ $conn->close();
     </section>
 
   </main>
+  <?php include "footer.php"; ?>
 
   <!-- Vendor JS Files -->
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
---
+++ footer.php
@@ -0,0 +1,61 @@
+
+<footer>
+        <div class="footer-cointaner">
+            <div class="row">
+                  <div class="col" id="company">
+                      <img src="logo.png" alt="" class="logo">
+                      <p>
+                        We are specialized in designings, make your business a brand.
+                        Try our premium services.
+                      </p>
+                      <div class="social">
+                        <a href="#"><i class="fab fa-facebook"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-youtube"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                      </div>
+                  </div>
+
+
+                  <div class="col" id="services" style="margin-top: -70px;">
+                     <h3>Services</h3>
+                     <div class="links">
+                        <a href="#">AC Services</a>
+                        <a href="#">Cleaning Services</a>
+                        
+                     </div>
+                  </div>
+
+                  <div class="col" id="useful-links">
+                     <h3>Links</h3>
+                     <div class="links">
+                        <a href="#">Home</a>
+                        <a href="#">Services</a>
+                        <a href="#">About</a>
+                        <a href="#">Contact</a>
+                        <a href="#">Feature</a>
+                     </div>
+                  </div>
+
+                  <div class="col" id="contact">
+                      <h3>Contact</h3>
+                      <div class="contact-details">
+                         <i class="fa fa-location"></i>
+                         <p> Amwaj Building , Burdubai, Office No.
+                            G19, P.O Box: 46777</p>
+                      </div><br><br>
+                      <div class="contact-details">
+                         <i class="fa fa-phone"></i>
+                         <p>+971 (04) 357 2148</p>
+                      </div><br><br>
+                      <div class="contact-details">
+                        <i class="fa fa-envelope"></i>
+                       <p><a href="mailto:info@bbcmaintenance.com" style="color: black;">info@bbcmaintenance.com</a></p>
+
+                    </div>
+                  </div>
+            </div>
+
+        </div>
+     </footer>
