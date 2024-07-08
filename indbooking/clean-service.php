b7a1573ac998caf057a7f0d3358250ebee4079b5
sabol72 <sabal72sc@gmail.com>
2024-07-08 12:00:32 +0545

    booking changes

---
+++ indbooking/clean-service.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Choosing Service Type</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+    <link href="Style.css" rel="stylesheet">
+    <link href="../assets/css/main.css" rel="stylesheet">
+
+    <style>
+    * {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+
+body{
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #350048;
+
+}
+</style>
+</head>
+<body>
+    <header>
+</header>
+    <main>
+        <div class="container mt-5 title-section" data-aos="fade-up">
+        <h2>Choose your service</h2>
+            <div class="buttons">
+                <a class="gbutton"href="#">Deep Cleaning Service</a>
+                <a class="gbutton"href="#">HV/AC Air Duct Cleaning Service</a>
+                <a class="gbutton"href="#">Upholstery Sofa Steam Cleaning</a>
+                <a class="gbutton"href="#">Villa & House Cleaning</a>
+                <a class="gbutton"href="#">Water Tank Cleaning & Disinfection</a>
+                <a class="gbutton"href="#">Window Glass Cleaning</a>
+                <a class="gbutton"href="#">Matress Cleaning</a>
+                <a class="gbutton"href="#">Carpet Cleaning</a>
+                <a class="gbutton"href="#">Pool Cleaning</a>
+
+            </div>
+        </div>
+    </main>
+    <script src="Script.js">
+  </script>
+</body>
+</html>
