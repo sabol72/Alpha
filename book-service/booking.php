093df57f492edd8efa854dba60a9afab08ae15b8
sabol72 <sabal72sc@gmail.com>
2024-07-09 17:00:22 +0545

    ALA

--- book-service/booking.php
+++ book-service/booking.php
@@ -9,7 +9,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <!-- <link rel="stylesheet" href="../assets/css/main.css"> -->
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Main.css">
 
 </head>
 
